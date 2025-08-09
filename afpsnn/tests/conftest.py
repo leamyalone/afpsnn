3a5d8fb5ad5f530efaf087fbf8552645a02c5c38
leamyalone <leamyalone4@yahoo.com>
2025-08-09 09:12:58 -0700

    Create conftest.py

---
+++ afpsnn/tests/conftest.py
@@ -0,0 +1,9 @@
+import os, random, numpy as np
+import pytest
+
+@pytest.fixture(autouse=True)
+def _global_seeds():
+    random.seed(0)
+    np.random.seed(0)
+    os.environ.setdefault("AFPSNN_SEED","0")
+    yield
